424bf26db73a82851000aab63ef7a629dee3ff69
anugrah-batch7 <gambanggambangsusah@gmail.com>
2021-10-03 14:48:31 +0700

    Fixing Dictionaries in MailServiceString

--- src/main/java/com/enigma/bankenigma/string_properties/MailServiceString.java
+++ src/main/java/com/enigma/bankenigma/string_properties/MailServiceString.java
@@ -8,11 +8,10 @@ public class MailServiceString {
                     "\n" +
                     "\n" +
                     "Berikut adalah link untuk registrasi Enigma E-Payment. " +
-                    "mohon diperhatikan bahwa link akan expire dalam waktu 5 menit" +
+                    "mohon diperhatikan bahwa link akan kadaluarsa dalam waktu 5 menit" +
                     "\n" +
                     "\n" +
                     "%s " +
-                    "\n" +
                     "\n" +
                     "terima kasih telah menggunakan layanan kami";
     public static final String OTP_LINK_BODY = "http://localhost:8081/authenticate/%s";
